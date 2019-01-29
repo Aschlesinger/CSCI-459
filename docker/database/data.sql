0683d58d6d34fb2043457f2d1b09e0cca121ea9d
Alexander Schlesinger <schlesingerab@g.cofc.edu>
2019-01-28 21:51:23 -0500

    added add and delete to database with the addtion of other pages for support

--- docker/database/data.sql
+++ docker/database/data.sql
@@ -1,7 +1,7 @@
 
-DROP TABLE IF EXISTS `dockerSample`;
+DROP TABLE IF EXISTS `data459`;
 
-CREATE TABLE `dockerSample` (
+CREATE TABLE `data459` (
   `id` varchar(6) DEFAULT NULL,
   `f_name` varchar(20) DEFAULT NULL,
   `l_name` varchar(20) DEFAULT NULL
@@ -9,11 +9,11 @@ CREATE TABLE `dockerSample` (
 
 
 
-LOCK TABLES `dockerSample` WRITE;
 
-INSERT INTO `dockerSample` (id,f_name, l_name)
+
+INSERT INTO `data459` (id,f_name, l_name)
  VALUES ('1','George','bill'),
- ('2','Alexander','Schlesinger'),
+ ('8','Alexander','Schlesinger'),
  ('3','Dank','Smith'),
  ('4','Billy','Bob'),
  ('5','Gandalf','Gray'),
@@ -23,4 +23,4 @@ INSERT INTO `dockerSample` (id,f_name, l_name)
  ('66','Execute','Order'),
  ('117','Master','Cheif')
   ;
-UNLOCK TABLES;
+/*LOAD DATA LOCAL INFILE '/database/data.txt' INTO TABLE data459;*/
